e41d27f4bf2c279fdada5c4dfd9228f684ed33c4
Luís Felipe de Oliveira <luis252915@gmail.com>
2024-09-04 16:56:44 -0300

    Update ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -8,13 +8,13 @@ public class ContaTerminal {
             System.out.println("Por favor, digite seu nome inteiro: ");
             String nome = scanner.nextLine();
 
-            System.out.println("Por favor, digite o numero da conta: ");
+            System.out.println("Por favor, digite o número da conta: ");
             int conta = scanner.nextInt();
 
-            System.out.println("Por favor, prossiga inserindo o numero da agencia: ");
+            System.out.println("Por favor, prossiga inserindo o numero da agência: ");
             String agencia = scanner.next();
 
-            System.out.println("Por ultimo, por favor, insira seu saldo: ");
+            System.out.println("Por último, por favor, insira seu saldo: ");
             Double saldo = scanner.nextDouble();
 
             System.out.println ("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + conta + " e seu saldo de: " + saldo + " já esta disponível para saque.");
